7c33c484b57de9c1bd6f3ecb995d2456415eb4d7
Marcio Schatt <jrschatt12@hotmail.com>
2022-10-10 12:59:13 -0300

    Exercício 4

---
+++ src/App.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner bimestral = new Scanner(System.in);
+
+        System.out.println("Digite as 4 notas bimestrais:");
+
+        float nota1 = bimestral.nextFloat();
+        float nota2 = bimestral.nextFloat();
+        float nota3 = bimestral.nextFloat();
+        float nota4 = bimestral.nextFloat();
+        float media = (nota1 + nota2 + nota3 + nota4) / 4;
+        bimestral.close();
+
+        System.out.println("A média total das notas é: " + media);
+    }
+}
+
